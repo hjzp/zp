7f330bf18efcbe602151ccabd5c9965bf86d9d5b
hjzp <3391659834@qq.com>
2024-04-09 14:18:55 +0800

    first file

---
+++ src/main/java/com/example/dao/Impl/UserDaoImpl.java
@@ -0,0 +1,19 @@
+package com.example.dao.Impl;
+
+import com.example.dao.UserDao;
+import com.example.entity.User;
+import com.example.until.JDBCUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+    @Override
+    public List<User> findAll() {
+        String sql = "select * from user";
+        List<User> users = template.query(sql, new BeanPropertyRowMapper<User>(User.class));
+        return users;
+    }
+}
---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String name;
+    private String gender;
+    private Integer age;
+    private String address;
+    private String qq;
+    private String email;
+}
